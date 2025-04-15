dc6ac37d78e9ba7cef22daeb15c8c9360f683f96
rohitvk09 <justlogin49@gmail.com>
2025-04-15 10:04:42 +0530

    File Saved

---
+++ number.java
@@ -0,0 +1,21 @@
+package Devops.CIE;
+
+import java.util.Scanner;
+
+public class number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter the number: ");
+        double number = scanner.nextDouble(); 
+
+        if (number > 0) {
+            System.out.println("The number is positive");
+        } else if (number == 0) {
+            System.out.println("The number is ZERO");
+        } else {
+            System.out.println("The number is negative");
+        }
+
+        scanner.close(); 
+    }
+}
